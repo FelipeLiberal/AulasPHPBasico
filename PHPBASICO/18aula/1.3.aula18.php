749314aa5a394c2b748eff6cf55cc2078508c049
FelipeLiberal <felipeliberal1@gmail.com>
2021-11-11 13:40:44 -0300

    Aula 18 do Curso Básico de PHP Curso em Vídeo
    Professor: Gustavo Guanabara

---
+++ PHPBASICO/18aula/1.3.aula18.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $v = array( 0=>5,
+                    1=>9,
+                    2=>8,
+                    3=>7);
+        unset($v[0]);
+        print_r($v);
+    ?>
+</body>
+</html>
